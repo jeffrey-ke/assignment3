01d403380ad223877a10fcfff3f2d53c30b683fa
jeff <jke3@cs.cmu.edu>
2025-10-29 20:54:06 -0400

    q3 done

--- q3.py
+++ q3.py
@@ -24,16 +24,11 @@ def algebraic_triangulate(correspondences, cameras):
         A_i[2] = a_prime * P_prime[2] - c_prime * P_prime[0]
         A_i[3] = b_prime * P_prime[2] - c_prime * P_prime[1]
 
-        return A_i
+        *_, Vh = np.linalg.svd(A_i)
+        return Vh[-1]
 
-    A = np.concatenate(
-        [create_row(correspondence) for correspondence in correspondences.transpose(1, 0, 2)],
-        axis=0
-    )
-    pdb.set_trace()
-    *_, Vh = np.linalg.svd(A)
-
-    return utils.unhomogenize(Vh[-1].reshape(-1, 4), keepdims=True)
+    points = np.array([solve_A(correspondence) for correspondence in correspondences.transpose(1, 0, 2)])
+    return utils.unhomogenize(points, keepdims=True)
 
 def triangulate(correspondences, cameras):
     P, P_prime = cameras
@@ -54,19 +49,23 @@ def triangulate(correspondences, cameras):
         projections_P, projections_P_prime = utils.unhomogenize(
             np.einsum('cij,bj->cbi', cameras, points_3d)
         )
-        all_x, all_x_prime = utils.unhomogenize(all_x), utils.unhomogenize(all_x_prime)
-        distance = np.linalg.norm(projections_P - all_x, axis=-1) + np.linalg.norm(projections_P_prime - all_x_prime)
+        all_x_copy, all_x_prime_copy = utils.unhomogenize(all_x), utils.unhomogenize(all_x_prime)
+        distance = np.linalg.norm(projections_P - all_x_copy, axis=-1) + np.linalg.norm(projections_P_prime - all_x_prime_copy)
         assert len(distance.shape) == 1
         return distance
 
-    result = least_squares(residual, init_3d_points)
+    jac_sparsity = np.zeros((1000, 4000))
+    for i in range(1000):
+        jac_sparsity[i, i*4:(i+1)*4] = 1
+    result = least_squares(residual, init_3d_points, jac_sparsity=jac_sparsity)
     assert result.success
-    return result.x.reshape(-1, 3)
+    return result.x.reshape(-1, 4)
 
 @dataclass
 class Q3:
     img1: np.ndarray
     img2: np.ndarray
+    colors: np.ndarray
     corresp: np.ndarray
     cameras: np.ndarray
 
@@ -75,16 +74,22 @@ def load_q3():
     img1 = cv2.imread(os.path.join(root, "img1.jpg"))
     img2 = cv2.imread(os.path.join(root, "img2.jpg"))
     assert img1 is not None and img2 is not None
-    rng = np.random.default_rng(0)
-    pts1 = rng.choice(np.load(os.path.join(root, "pts1.npy")).astype(np.float32), 1000)
-    pts2 = rng.choice(np.load(os.path.join(root, "pts2.npy")).astype(np.float32), 1000)
+    pts1 = np.load(os.path.join(root, "pts1.npy")).astype(np.float32)
+    pts2 = np.load(os.path.join(root, "pts2.npy")).astype(np.float32)
+    idxs = np.random.default_rng(0).choice(np.arange(pts1.shape[0]), 1000, replace=False)
+    pts1 = pts1[idxs]
+    pts2 = pts2[idxs]
+    colors = img1[pts1[:, 1].astype(int), pts1[:, 0].astype(int)]
     P = np.load(os.path.join(root, "P1.npy"))
     P_prime = np.load(os.path.join(root, "P2.npy"))
     corresps = utils.homogenize(np.stack((pts1, pts2), axis=0))
     cameras = np.stack((P, P_prime), axis=0)
-    return Q3(img1, img2, corresps, cameras)
+    return Q3(img1, img2, colors, corresps, cameras)
 
 if __name__ == "__main__":
     q3 = load_q3()
+    os.makedirs("output/q3", exist_ok=True)
     points_3d = triangulate(q3.corresp, q3.cameras)
-    pdb.set_trace()
+    colors = q3.colors
+    np.savez("output/q3/points3d.npz", points_3d=points_3d, colors=colors)
+    print("done!")
